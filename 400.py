75949890a9a5020966547cd40f3b4caf0f9d10f3
Zaid Al-Qadiri <zaidalqadiri@Zaids-MacBook-Air.local>
2025-06-10 17:39:44 -0600

    Created Deck Class

--- 400.py
+++ 400.py
@@ -8,3 +8,24 @@ class Card:
     def __str__(self):
         return f"{self.__rank} of {self.__suit}"
     
+class Deck:
+    def __init__(self):
+        suits = ['Diamond', 'Heart', 'Spade', 'Club']
+        ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+
+        self.__deck = []
+
+        for suit in suit:
+            for rank in ranks:
+                self.__deck.append(Card(suit, rank))
+
+        random.shuffle(self.__deck)
+    
+    def deal(player):
+        pass
+
+
+    
+
+
+    
