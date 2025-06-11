5c082092f17eb95fa460d5f16398cf920cac7cc9
Zaid Al-Qadiri <zaidalqadiri@Zaids-MacBook-Air.local>
2025-06-10 18:52:45 -0600

    created game class

--- 400.py
+++ 400.py
@@ -50,19 +50,31 @@ class Player:
 class Team:
     _team_count = 0
 
-    def __init__(self):
-        self.__players = []
+    def __init__(self, player1, player2):
+        self.__players = [player1, player2]
         Team._team_count += 1
         self.__team_id = Team._team_count
-    
-    def add_members(self, player1, player2):
-        if len(self.__players) < 2:
-            self.__players.append(player1, player2)
-        else:
-            print("This team is already full")
     
     def get_team_info(self):
         return f"Team {self.__team_id}: {self.__players[0]} and {self.__players[1]}"
 
+class Game:
 
+    _current_round = 0
+
+    def __init__(self, player1, player2, player3, player4, team1, team2):
+        self.__players = [player1, player2, player3, player4]
+        self.__deck = Deck().__deck
+        self.__teams = [team1, team2]
+
+    def start_game():
+        pass
     
+    def bid_phase():
+        pass
+
+    def play_round():
+        pass
+
+    def evaluate_tricks():
+        pass 
